34868e2e9154da2abf5c0fd542e7b4a87124f609
Marcus Guthrie <marcusguthrie111@gmail.com>
2017-09-18 22:05:15 +0100

    timings as object

--- cuppaTea.js
+++ cuppaTea.js
@@ -51,29 +51,33 @@ var dataController = (function() {
       mild: [30, 100],
       hot: [0, 0]
     },
-    cupColour: {
+    darkness: {
       white: [0, 0],
       black: [30, 60],
       mediumDarkness: [20, 40]
-    }
+    },
+    timer: [465, 1500]
   };
 
   return {
-    addVariables: function(type, surface, mlk, mlkAmount, rmTmp, clr, num) {
-      // duration (in ms) for each category, stored in array
-      var timerArr = [tea.cupType[type][num], tea.cupSurfaceArea[surface][num], tea.milk[mlk][num], tea.milkAmount[mlkAmount][num], tea.roomTemp[rmTmp][num], tea.cupColour[clr][num]];
-      console.log(timerArr);
-      return timerArr;
-
+    addVariables: function(type, surface, mlk, mlkAmount, rmTmp, dark, tmr, num) {
+      var timerObj = {
+        type: tea.cupType[type][num],
+        surface: tea.cupSurfaceArea[surface][num],
+        milk: tea.milk[mlk][num],
+        milkAmount: tea.milkAmount[mlkAmount][num],
+        roomTemp: tea.roomTemp[rmTmp][num],
+        darkness: tea.darkness[dark][num],
+        timer: tea.timer[num]
+      };
+      return timerObj;
     },
 
-    calcTimer: function(maxDuration, timings) {
+    calcTimer: function(obj) {
       var timeSecs, timeMs;
-      time = maxDuration - (timings[0] + timings[1] + timings[2] + timings[3] + timings[4] + timings[5])
-      console.log(time);
+      time = obj.timer - (obj.type + obj.surface + obj.milk + obj.milkAmount + obj.roomTemp + obj.darkness);
       // time is in seconds
       return time;
-
     }
 
   };
@@ -86,32 +90,18 @@ var dataController = (function() {
 var UIController = (function() {
   var percent, fly, brewCount, coldCount, animation;
 
-  // Finish the DOMstrings for rest of javascript
-  var DOMstrings = {
-    setTimer: 'set_timer',
-    reset: 'reset',
-    cupType: 'cup_type',
-    surface: 'surface',
-    milk: 'milk',
-    milkAmount: 'milk_amount',
-    roomTemp: 'room_temp',
-    cupColour: 'cup_colour',
-    timer: 'select_timers',
-    countDown: 'countdown'
-  };
-
-  var changeSpillClr = function(spillClass) {
+  var changeSpilldark = function(spillClass) {
     var spillArr;
-    spillArr = document.getElementsByClassName('spill')
+    spillArr = document.getElementsByClassName('spill');
     for (var i = 0; i < spillArr.length; i++) {
       spillArr[i].className = 'spill' + ' ' + spillClass;
     }
   };
 
   var formatTime = function(time) {
-    var mins, secs, minsDecimal, minsSplit, secsFraction, minsSecs
-    mins = Math.floor(time / 60)
-    secs = time % 60
+    var mins, secs, minsSecs;
+    mins = Math.floor(time / 60);
+    secs = time % 60;
     minsSecs = mins + ' minutes ' + secs + ' seconds';
     return minsSecs;
   };
@@ -122,15 +112,14 @@ var UIController = (function() {
 
   return {
 
-    adjustImage: function(type, surface, mlk, mlkAmount, rmTmp, clr) {
-      console.log(type, surface, mlk, mlkAmount, rmTmp, clr);
+    adjustImage: function(type, surface, mlk, mlkAmount, rmTmp, dark) {
       if (type === 'glass') {
-        // blank out cup colour option + hide handle
-        document.getElementById('cup_colour').value = 'white';
-        document.getElementById('cup_colour').disabled = true;
+        // blank out cup darkness option + hide handle
+        document.getElementById('darkness').value = 'white';
+        document.getElementById('darkness').disabled = true;
         document.querySelector('.handle').style.display = 'none';
       } else {
-        document.getElementById('cup_colour').disabled = false;
+        document.getElementById('darkness').disabled = false;
         document.querySelector('.handle').style.display = '';
       };
       if (mlk === 'none') {
@@ -141,13 +130,11 @@ var UIController = (function() {
       }
       // change class names according to options
       document.querySelector('.tea').className = 'tea' + ' ' + surface + ' ' + mlkAmount;
-      document.querySelector('.cup').className = 'cup' + ' ' + type + ' ' + surface + ' ' + clr;
-      document.querySelector('.handle').className = 'handle' + ' ' + type + ' ' + surface + ' ' + clr;
+      document.querySelector('.cup').className = 'cup' + ' ' + type + ' ' + surface + ' ' + dark;
+      document.querySelector('.handle').className = 'handle' + ' ' + type + ' ' + surface + ' ' + dark;
       document.querySelector('.ripple').className = 'ripple' + ' ' + mlkAmount;
-      changeSpillClr(mlkAmount);
+      changeSpilldark(mlkAmount);
       document.body.className = rmTmp;
-
-
     },
 
 
@@ -160,14 +147,14 @@ var UIController = (function() {
       } else if (document.getElementById('both').checked) {
         timerValue = document.getElementById('both').value;
       }
-      console.log(timerValue);
+
       return {
-        cupType: document.getElementById(DOMstrings.cupType).value,
-        surface: document.getElementById(DOMstrings.surface).value,
-        milk: document.getElementById(DOMstrings.milk).value,
-        milkAmount: document.getElementById(DOMstrings.milkAmount).value,
-        roomTemp: document.getElementById(DOMstrings.roomTemp).value,
-        cupColour: document.getElementById(DOMstrings.cupColour).value,
+        cupType: document.getElementById('cup_type').value,
+        surface: document.getElementById('surface').value,
+        milk: document.getElementById('milk').value,
+        milkAmount: document.getElementById('milk_amount').value,
+        roomTemp: document.getElementById('room_temp').value,
+        darkness: document.getElementById('darkness').value,
         timer: timerValue
       }
     },
@@ -180,43 +167,38 @@ var UIController = (function() {
       percent = 0;
       ms = time * 10;
       brewCount = setInterval(frame, ms);
-      brewDisplay.innerHTML = 'Cooling down. Be patient...' + percent * 1 + '% ready';
+      brewDisplay.innerHTML = 'Brewing... ' + percent * 1 + '% ready';
       // function frame peforms each interval
       function frame() {
         percent++;
         if (percent == 100) {
           brewDisplay.innerHTML = 'Drink Up! ' + percent * 1 + '% ready';
           clearInterval(brewCount);
-        } else if (percent > 2 && percent < 8) {
-          brewDisplay.innerHTML = 'Watch out for the fly... ' + percent * 1 + '% ready';
-        } else if (percent > 10 && percent < 28) {
-          brewDisplay.innerHTML = 'Mmmmmmm Tea... ' + percent * 1 + '% ready';
         } else {
-          brewDisplay.innerHTML = 'Cooling down. Be patient... ' + percent * 1 + '% ready';
+          brewDisplay.innerHTML = 'Brewing... ' + percent * 1 + '% ready';
         }
-      }
+      };
     },
 
     coldTimer: function(time) {
       var displayTime = formatTime(time);
-      coldDisplay = document.getElementById('cold_display')
-      coldDisplay.innerHTML = displayTime;
+      coldDisplay = document.getElementById('cold_display');
+      coldDisplay.innerHTML = 'Reminder it\'s getting cold in ' + displayTime;
       coldCount = setInterval(coldCountDown, 1000);
 
       function coldCountDown() {
         time--;
         displayTime = formatTime(time);
-        coldDisplay.innerHTML = displayTime;
+        coldDisplay.innerHTML = 'Reminder it\'s getting cold in ' + displayTime;
         if (time === 0) {
           clearInterval(coldCount);
           alert('Drink Up your tea\'s getting cold!')
         }
-      }
+      };
     },
 
     progressBarWidth: function(wrapper) {
-      document.getElementById(wrapper).style.width = '100%'
-
+      document.getElementById(wrapper).style.width = '100%';
     },
 
     progressBarWidthBoth: function(brew, cold) {
@@ -226,8 +208,7 @@ var UIController = (function() {
 
     progressBarAnimate: function(time, divId) {
       var width, timeInMs;
-      width = document.getElementById(divId).offsetWidth
-      console.log(width);
+      width = document.getElementById(divId).offsetWidth;
       timeInMs = time * 1000;
       animation = document.querySelector('.bar').animate({
         width: ['0px', width + 'px']
@@ -264,7 +245,7 @@ var UIController = (function() {
 
 
 var controller = (function(dataCtrl, UICtrl) {
-  var input, dom;
+  var input, timeBrew, timeCold;
 
   var setupEventListeners = function() {
     var optionsArr;
@@ -273,16 +254,15 @@ var controller = (function(dataCtrl, UICtrl) {
     optionsArr.forEach(function(element) {
       element.addEventListener('click', changeImage);
     });
-    dom = UICtrl.getDOMstrings();
-    document.getElementById(dom.setTimer).addEventListener('click', setTimers);
-    document.getElementById(dom.reset).addEventListener('click', reset);
+    document.getElementById('set_timer').addEventListener('click', setTimers);
+    document.getElementById('reset').addEventListener('click', reset);
   };
 
   var changeImage = function() {
     // Get field data
     inputData();
     // Display new image according to input object
-    UICtrl.adjustImage(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.cupColour)
+    UICtrl.adjustImage(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.darkness)
   };
 
 
@@ -292,43 +272,31 @@ var controller = (function(dataCtrl, UICtrl) {
     // Get field data. Input variable will be an object with choices selected
     inputData();
     // Get time data according to variables
-    var timerArr1 = dataCtrl.addVariables(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.cupColour, 0);
-    var timerArr2 = dataCtrl.addVariables(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.cupColour, 1);
-
-    // Calculate times
-    var timeBrew = dataCtrl.calcTimer(465, timerArr1);
-    var timeCold = dataCtrl.calcTimer(1380, timerArr2);
-    var timeBoth = timeBrew + timeCold
-    console.log(timeBoth);
+    var brewObj = dataCtrl.addVariables(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.darkness, input.timer, 0);
+    var coldObj = dataCtrl.addVariables(input.cupType, input.surface, input.milk, input.milkAmount, input.roomTemp, input.darkness, input.timer, 1);
 
     if (input.timer === 'brew') {
+      // Calculate times
+      timeBrew = dataCtrl.calcTimer(brewObj);
+      console.log('Brew time ' + timeBrew + ' secs' );
       // Start and display first timer progress and alert when complete
       UICtrl.progressBarWidth('brew_wrapper');
       UICtrl.brewTimer(timeBrew, input.milkAmount);
       UICtrl.progressBarAnimate(timeBrew, 'brew_wrapper');
     } else if (input.timer === 'cold') {
-      // Start/display second timer time, same time as first - progress not visible
+      timeCold = dataCtrl.calcTimer(coldObj);
       UICtrl.progressBarWidth('cold_wrapper');
       UICtrl.coldTimer(timeCold);
       UICtrl.progressBarAnimate(timeCold, 'cold_wrapper');
     } else if (input.timer === 'both') {
+      timeBrew = dataCtrl.calcTimer(brewObj);
+      timeCold = dataCtrl.calcTimer(coldObj);
+      console.log('Brew time ' + timeBrew + ' secs' );
       UICtrl.progressBarWidthBoth(timeBrew, timeCold)
       UICtrl.brewTimer(timeBrew, input.milkAmount);
       UICtrl.coldTimer(timeCold);
       UICtrl.progressBarAnimateBoth(timeCold);
-
     }
-
-
-
-
-    /* To do:
-    - finished tea button - cancels second timer
-    - disable options whilst timing
-    -
-
-    */
-
   };
 
   var reset = function() {
@@ -337,19 +305,18 @@ var controller = (function(dataCtrl, UICtrl) {
   };
 
   var enableTimerBtn = function() {
-    document.getElementById(dom.reset).disabled = true;
-    document.getElementById(dom.setTimer).disabled = false;
+    document.getElementById('reset').disabled = true;
+    document.getElementById('set_timer').disabled = false;
   };
 
   var disableTimerBtn = function() {
-    document.getElementById(dom.reset).disabled = false;
-    document.getElementById(dom.setTimer).disabled = true;
+    document.getElementById('reset').disabled = false;
+    document.getElementById('set_timer').disabled = true;
   }
 
   var inputData = function() {
     // Input variable is object according to choices
     input = UICtrl.getInput();
-    console.log(input);
   };
 
   return {
